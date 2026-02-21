bc977383e2dbd49ae911efb90fbaeb96dd2e270a
Atul Kumar Jha <atuljha21542@gmail.com>
2026-02-22 00:42:14 +0530

    Fixed the server and made it runnable

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  
+  console.log(`🏦 LedgerPay is LIVE on port ${PORT}`);
+  console.log(`📖 Test the API at: http://localhost:${PORT}/api-docs`);
+});
