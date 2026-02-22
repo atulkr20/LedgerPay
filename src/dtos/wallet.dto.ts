1cb9a267e7d3c94f5525aef02f12cc068cac1d4b
Atul Kumar Jha <atuljha21542@gmail.com>
2026-02-22 12:25:49 +0530

    Adde Zod validation for money transfers

---
+++ src/dtos/wallet.dto.ts
@@ -0,0 +1,42 @@
+import { z } from 'zod';
+import { Request, Response, NextFunction } from 'express';
+
+// Validation Middleware
+
+export const validate = (schema: z.AnyZodObject) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try{
+            req.body = schema.parse(req.body);
+            next();
+        } catch (error: any) {
+            // 400 if user sent invalid data
+            return res.status(400).json({ error: error.errors });
+        }
+
+    };
+};
+
+// Strict rules for each route
+export const CreateWalletSchema = z.object({
+    userId: z.string().min(1, "User ID cannot be empty"),
+});
+
+export const AmountSchema = z.object({
+    accountId: z.string().uuid("Must be a valid uuid"),
+    // Money must be strictly positive. No zero or negative transfers
+    amount: z.number().positive("Amount must be greater than 0"),
+});
+
+export const TransferSchema = z.object({
+    fromAccountId: z.string().uuid(),
+    toAccountId: z.string().uuid(),
+    amount: z.number().positive("Amount must be greater than 0"),
+}).refine(data=> data.fromAccountId !== data.toAccountId, {
+    // Preventing users from sending money to themselves
+    message: "Sender and receiver cannot be the same account",
+    path: ["toAccountId"]
+});
+
+export const RefundSchema = z.object({
+    originalTransactionId: z.string().uuid("Must be a valid UUID"),
+});
