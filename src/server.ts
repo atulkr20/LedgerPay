f8d9bec05fbdf9560153a6fac8fc6a021b19061b
Atul Kumar Jha <atuljha21542@gmail.com>
2026-07-06 17:03:02 +0530

    Updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
+import http from 'http';
 import { app } from './app';
 import dotenv from 'dotenv';
+import { setupTerminal } from './terminal/terminal.routes';
 
 dotenv.config({ quiet: true });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+ const server = http.createServer(app);
+
+ const terminalPage = setupTerminal(server);
+
+ app.get('/terminal', terminalPage);
+
+server.listen(PORT, () => {
   
   console.log(` LedgerPay is LIVE on port ${PORT}`);
+  console.log(`CLI Terminal at: http://localhost:${PORT}/terminal`)
   console.log(` Test the API at: http://localhost:${PORT}/api-docs`);
 });
